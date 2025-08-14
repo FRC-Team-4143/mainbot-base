cedbadc0adcb58bf439dd3f48735930f872a686f
ColeHunt <huntcr2001@gmail.com>
2025-08-14 02:04:37 -0500

    adds superstruct and elevator io

---
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems.superstructure;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.superstructure.arm.ArmIO;
+import frc.robot.subsystems.superstructure.arm.ArmIOInputsAutoLogged;
+import frc.robot.subsystems.superstructure.arm.ArmIOSim;
+import frc.robot.subsystems.superstructure.arm.ArmIOTalonFX;
+import frc.robot.subsystems.superstructure.elevator.ElevatorConstants;
+import frc.robot.subsystems.superstructure.elevator.ElevatorIO;
+import frc.robot.subsystems.superstructure.elevator.ElevatorIOInputsAutoLogged;
+import frc.robot.subsystems.superstructure.elevator.ElevatorIOSim;
+import frc.robot.subsystems.superstructure.elevator.ElevatorIOTalonFX;
+
+public class Superstructure extends SubsystemBase {
+
+  private static Superstructure instance_ = null;
+
+  public static Superstructure getInstance() {
+    if (instance_ == null) {
+      if (Constants.IS_ROBOT_REAL) {
+        // Initialize with real IO components
+        instance_ =
+            new Superstructure(
+                new ElevatorIOTalonFX(ElevatorConstants.ELEVATOR_CONFIG), new ArmIOTalonFX());
+      } else {
+        // Initialize with simulated IO components
+        instance_ = new Superstructure(new ElevatorIOSim(), new ArmIOSim());
+      }
+    }
+    return instance_;
+  }
+
+  // Desired states for the superstructure
+  public enum WantedState {}
+
+  // Current system states for the superstructure
+  public enum SystemState {}
+
+  private final ElevatorIO elevator_io_;
+  private final ArmIO arm_io_;
+
+  private final ElevatorIOInputsAutoLogged elevator_io_inputs_auto_logged_ =
+      new ElevatorIOInputsAutoLogged();
+  private final ArmIOInputsAutoLogged arm_io_inputs_auto_logged_ = new ArmIOInputsAutoLogged();
+
+  Superstructure(ElevatorIO elevatorIO, ArmIO armIO) {
+    this.elevator_io_ = elevatorIO;
+    this.arm_io_ = armIO;
+  }
+}
