b39a4a920dd91e5035e48c0c6055065ef0ad6f1b
Cole Tucker <coalman321@users.noreply.github.com>
2025-10-04 22:47:04 -0500

    Make subsystem name available to I/O

--- src/main/java/frc/mw_lib/subsystem/SubsystemIO.java
+++ src/main/java/frc/mw_lib/subsystem/SubsystemIO.java
@@ -2,9 +2,28 @@ package frc.mw_lib.subsystem;
 
 public abstract class SubsystemIO<ConstantsType extends MWConstants> {
   protected final ConstantsType CONSTANTS;
+  private final String subsystem_name_;
 
   public SubsystemIO(ConstantsType constants) {
     CONSTANTS = constants;
+
+    // Use some Java magic to pull the class name
+    String name = this.getClass().getSimpleName();
+    name = name.substring(name.lastIndexOf('.') + 1);
+    if (name.contains("IO")) {
+      name = name.substring(0, name.indexOf("IO", 0));
+    }
+
+    subsystem_name_ = name;
+  }
+
+  /**
+   * a common logging key to use for the subsystem
+   *
+   * @return the logging key to use as a base. eg getSubsystemKey() + "Some Special Value"
+   */
+  public String getSubsystemKey() {
+    return "Subsystem/" + subsystem_name_ + "/";
   }
 
   public abstract void readInputs(double timestamp);
