efbba6b52eccbf9c5cf576293b8bdeaaee50514d
Cole Tucker <coalman321@users.noreply.github.com>
2025-10-14 20:15:02 -0500

    new FX configs

--- src/main/java/frc/mw_lib/mechanisms/ElevatorMech.java
+++ src/main/java/frc/mw_lib/mechanisms/ElevatorMech.java
@@ -5,30 +5,32 @@ import java.util.List;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import frc.mw_lib.mechanisms.FxMotorConfig.FxMotorType;
 
 public class ElevatorMech extends MechBase {
 
     // Always assume that we have the leader motor in index 0
-    private final TalonFX motors[];
-    private final PositionVoltage positionRequest;
-    private final VelocityVoltage velocityRequest;
+    private final TalonFX motors_[];
+    private final PositionVoltage position_request_;
+    private final VelocityVoltage velocity_request_;
 
     // Simulation
-    private final ElevatorSim elevatorSim;
+    private final ElevatorSim elevator_sim_;
 
-    public ElevatorMech(List<FxMotorConfig> motor_configs, double gearRatio, double drumRadius, double carriageMassKg,
-            double maxExtension) {
-        this(motor_configs, gearRatio, drumRadius, carriageMassKg, maxExtension, false);
+    public ElevatorMech(List<FxMotorConfig> motor_configs, double gear_ratio, double drum_radius, double carriage_mass_kg,
+    double max_extension, double rigging_ratio) {
+        this(motor_configs, gear_ratio, drum_radius, carriage_mass_kg, max_extension, rigging_ratio, false);
     }
 
-    public ElevatorMech(List<FxMotorConfig> motor_configs, double gearRatio, double drumRadius, double carriageMassKg,
-            double maxExtension, boolean is_vertical) {
+    public ElevatorMech(List<FxMotorConfig> motor_configs, double gear_ratio, double drum_radius, double carriage_mass_kg,
+            double max_extension, double rigging_ratio, boolean is_vertical) {
         super();
 
         // throw a fit if we don't have any motors
@@ -36,14 +38,14 @@ public class ElevatorMech extends MechBase {
             throw new IllegalArgumentException("Motor configs is null or empty");
         }
 
-        motors = new TalonFX[motor_configs.size()];
+        motors_ = new TalonFX[motor_configs.size()];
         for (int i = 0; i < motor_configs.size(); i++) {
             FxMotorConfig cfg = motor_configs.get(i);
             if (cfg.canbus_name == null || cfg.canbus_name.isEmpty()) {
                 throw new IllegalArgumentException("Motor canbus name is null or empty");
             }
 
-            motors[i] = new TalonFX(cfg.can_id, cfg.canbus_name);
+            motors_[i] = new TalonFX(cfg.can_id, cfg.canbus_name);
 
             // Only apply the configs to the first motor, the rest are followers
             if (i == 0) {
@@ -55,23 +57,34 @@ public class ElevatorMech extends MechBase {
                 }
 
                 // also force the gear ratio to be correct
-                cfg.config.Feedback.SensorToMechanismRatio = gearRatio;
+                cfg.config.Feedback.SensorToMechanismRatio = gear_ratio;
 
-                motors[i].getConfigurator().apply(cfg.config);
+                motors_[i].getConfigurator().apply(cfg.config);
+            } else {
+                // make the rest of the motors followers
+                motors_[i].setControl(new StrictFollower(motors_[0].getDeviceID()));
             }
         }
 
-        positionRequest = new PositionVoltage(0).withSlot(0);
-        velocityRequest = new VelocityVoltage(0).withSlot(1);
+        position_request_ = new PositionVoltage(0).withSlot(0);
+        velocity_request_ = new VelocityVoltage(0).withSlot(1);
+
+        DCMotor motor_type;
+        if(motor_configs.get(0).motor_type == FxMotorType.X60){
+            motor_type = DCMotor.getKrakenX60(motor_configs.size());
+        } else {
+            throw new IllegalArgumentException("Unsupported motor type");
+            // motor_type = DCMotor.getKr(motor_configs.size());
+        } 
 
         // construct the simulation object
-        elevatorSim = new ElevatorSim(
-                DCMotor.getKrakenX60(1), // Motor type
-                gearRatio,
-                carriageMassKg, // Carriage mass (kg)
-                drumRadius, // Drum radius (m)
+        elevator_sim_ = new ElevatorSim(
+                motor_type, // Motor type
+                gear_ratio,
+                carriage_mass_kg, // Carriage mass (kg)
+                drum_radius, // Drum radius (m)
                 0,
-                maxExtension, // Max height (m)
+                max_extension, // Max height (m)
                 is_vertical, // Simulate gravity
                 0 // Starting height (m)
         );
@@ -96,7 +109,7 @@ public class ElevatorMech extends MechBase {
     }
 
     public void setCurrentPosition(double position, double feedforward) {
-        motors[0].setPosition(position);
+        motors_[0].setPosition(position);
     }
 
     public double getPosition() {
--- src/main/java/frc/mw_lib/mechanisms/FxMotorConfig.java
+++ src/main/java/frc/mw_lib/mechanisms/FxMotorConfig.java
@@ -4,9 +4,16 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
 public class FxMotorConfig {
 
+    public enum FxMotorType {
+        X60,
+        X44
+    }
+
     public String canbus_name = "rio";
 
     public int can_id = 1;
+
+    public FxMotorType motor_type = FxMotorType.X60;
 
     public TalonFXConfiguration config = new TalonFXConfiguration();
     
