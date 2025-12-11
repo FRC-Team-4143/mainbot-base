127288348aa8c7b7e5f6cbbcc66e5eda25dc488b
Ben-Shoff <b.a.shoff@students.mths.us>
2025-12-10 19:23:03 -0600

    Added arm

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -13,19 +13,39 @@ import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.subsystems.swerve.Swerve;
 import java.util.Optional;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 public abstract class OI {
 
   // Sets up both controllers
   private static CommandXboxController driver_controller_ = new CommandXboxController(0);
+  private static TalonFX turret = new TalonFX(22);
+  private static TalonFX trunnion = new TalonFX(21);
+  private static TalonFX extend = new TalonFX(20);
 
   public static void configureBindings() {
     DriverStation.silenceJoystickConnectionWarning(true);
-
+    turret.setNeutralMode(NeutralModeValue.Brake);
+    trunnion.setNeutralMode(NeutralModeValue.Brake);
+    extend.setNeutralMode(NeutralModeValue.Brake);
     driver_controller_.rightStick().onTrue(Swerve.getInstance().toggleFieldCentric());
     // driver_controller_.a().whileTrue(Commands.startEnd(() -> Superstructure.getInstance().requestMove(Targets.L3), () -> Superstructure.getInstance().requestMove(Targets.CORAL_INTAKE)));
   
-    driver_controller_.a().onTrue(Commands.run(() -> ElevatorSubsystem.getInstance().requestMove(0.0)));
-    driver_controller_.b().onTrue(Commands.run(() -> ElevatorSubsystem.getInstance().requestMove(1.0)));
+    //driver_controller_.a().onTrue(Commands.run(() -> ElevatorSubsystem.getInstance().requestMove(0.0)));
+    //driver_controller_.b().onTrue(Commands.run(() -> ElevatorSubsystem.getInstance().requestMove(1.0)));
+    driver_controller_.x().whileTrue(Commands.startEnd(() -> turret.set(.2),
+                                                       () -> turret.set(0)));
+    driver_controller_.y().whileTrue(Commands.startEnd(() -> turret.set(-.2),
+                                                       () -> turret.set(0)));
+    driver_controller_.a().whileTrue(Commands.startEnd(() -> trunnion.set(.2),
+                                                       () -> trunnion.set(0)));
+    driver_controller_.b().whileTrue(Commands.startEnd(() -> trunnion.set(-.2),
+                                                       () -> trunnion.set(0)));
+    driver_controller_.leftBumper().whileTrue(Commands.startEnd(() -> extend.set(.6),
+                                                                () -> extend.set(0)));
+    driver_controller_.rightBumper().whileTrue(Commands.startEnd(() -> extend.set(-.6),
+                                                                 () -> extend.set(0)));
     SmartDashboard.putData("SeedFieldCentric", Commands.runOnce(Swerve.getInstance()::seedFieldCentric));
   }
 
